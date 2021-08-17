d6b0290d5dfa6ba6fee8d31beaeebee5cc003141
Katie DeMet <katie.demet7@gmail.com>
2021-08-17 11:17:57 -0500

    rollbar up

--- server.js
+++ server.js
@@ -6,7 +6,17 @@ app.use(express.json())
 
 const port = process.env.PORT || 4040
 
-app.get('/', function(req, res) {
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: '290b9a910c98447c8c942f3e817d1b20',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+rollbar.log('Hello world!')
+
+app.get('/', function(req, res) { 
+    rollbar.log('App.get hit')
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
